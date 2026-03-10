001b5292615f6bceba1c7223fd106bb9813295f9
lukxpo <lucas.r.esposito@gmail.com>
2026-03-10 15:33:39 -0300

    add player.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -17,6 +20,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
